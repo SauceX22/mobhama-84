71e1f812e521db4393ed2fcf1b9b2f55e726e0ba
SauceX <mahmoudzak4@gmail.com>
2023-12-11 22:57:34 +0300

    chore: basic project details page structure

--- frontend/src/app/(dashboard)/dashboard/projects/[projectId]/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/projects/[projectId]/page.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+import { DashboardHeader } from "@/components/header";
+import { DashboardShell } from "@/components/shell";
+
 type Props = {
   params: { projectId: string };
 };
 
 const ProjectDetailsPage = ({ params, ...props }: Props) => {
-  return <div>{params.projectId}</div>;
+  return (
+    <DashboardShell>
+      <DashboardHeader
+        // TODO get the project name using id
+        heading={`Project ${params.projectId}`}
+        text="Explore and manage project details."
+      />
+      <div className="grid gap-10">tests</div>
+    </DashboardShell>
+  );
 };
 
 export default ProjectDetailsPage;
