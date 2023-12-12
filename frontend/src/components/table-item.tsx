69c035bfa447a15d617081c213c2b2de00e6fc4d
SauceX <mahmoudzak4@gmail.com>
2023-12-12 21:49:14 +0300

    fix: some timetable shit

--- frontend/src/components/table-item.tsx
+++ frontend/src/components/table-item.tsx
@@ -1,18 +1,55 @@
 import React from "react";
-import { type Event } from "@/types/event";
 import { Icons } from "./icons";
+import { type Reservation } from "@/server/api";
+import { parse, set } from "date-fns";
+import { cn } from "@/lib/utils";
 
-const TableItem = ({ event }: { event: Event }) => {
+type TableItemProps = {
+  reservationId: string;
+};
+
+const TableItem = ({ reservationId }: TableItemProps) => {
+  // TODO fetch and mark loading
+  const reservation = {
+    id: "event-id",
+    machine: {
+      id: "machine-id",
+      name: "machine 1",
+    },
+    team: {
+      id: "team-id",
+      name: "team 1",
+      members: [],
+      projects: [],
+    },
+    startTime: set(parse("2023-12-12", "yyyy-MM-dd", new Date()), {
+      hours: 9,
+      minutes: 0,
+    }),
+    duration: 60,
+  };
+
   return (
     <div
-      className={`absolute w-full cursor-alias rounded bg-green-200 text-center text-[12px] dark:bg-green-900`}
+      className={cn(
+        "absolute h-16 w-full rounded-md bg-green-200 text-center text-[12px] text-white shadow-sm dark:bg-green-500",
+        // row span = int(duration/30) + 1
+        // Math.floor(reservation.duration / 30) + 1,
+      )}
+      style={{
+        gridRow:
+          Math.floor(reservation.duration / 30) +
+          (reservation.duration % 30 !== 0 ? 1 : 0),
+      }}
     >
       <div className="relative flex h-full w-full items-center justify-center">
         <div className="pointer-events-none absolute left-1 top-0 select-none">
-          {event.title}
+          team name
+          {/* {reservation.} */}
         </div>
         <div className="pointer-events-none absolute bottom-0 left-1 select-none">
-          {event.location}
+          machine name
+          {/* {reservation.machine.name} */}
         </div>
         <div
           className={`flex h-[1.11rem] w-[1.11rem] cursor-pointer select-none items-center justify-center rounded-full bg-white/50 opacity-50 hover:opacity-90 dark:bg-white/[0.15] dark:hover:opacity-100`}
@@ -20,10 +57,11 @@ const TableItem = ({ event }: { event: Event }) => {
           <Icons.close />
         </div>
         <div className="pointer-events-none absolute right-1 top-0 select-none">
-          {event.startTime}
+          {/* {reservation.startTime} */}
         </div>
         <div className="pointer-events-none absolute bottom-0 right-1 select-none">
-          {event.endTime}
+          end time
+          {/* {reservation.endTime} */}
         </div>
       </div>
     </div>
--- frontend/src/components/timetable.tsx
+++ frontend/src/components/timetable.tsx
@@ -1,8 +1,10 @@
 "use client";
-import React, { useState } from "react";
-import { cn } from "@/lib/utils";
-import { type Event } from "@/types/event";
+
+import React, { useEffect, useState } from "react";
+import { cn, getReservationHourRow } from "@/lib/utils";
 import TableItem from "@/components/table-item";
+import { type Reservation } from "@/server/api";
+import { getDay, parse, set } from "date-fns";
 
 const days = ["SUN", "MON", "TUE", "WED", "THU"];
 
@@ -11,10 +13,10 @@ const TimeTable: React.FC = () => {
   const rows = 11;
 
   // State to store events
-  const [events, setEvents] = useState<Event[]>([]);
+  const [events, setEvents] = useState<Reservation[]>([]);
 
   // Function to add an event
-  const addEvent = (newEvent: Event) => {
+  const addEvent = (newEvent: Reservation) => {
     // Update the events state
     setEvents([...events, newEvent]);
   };
@@ -24,7 +26,7 @@ const TimeTable: React.FC = () => {
     const rowElements = [];
     // Loop through each day
     for (let dayIndex = 0; dayIndex < days.length; dayIndex++) {
-      const day = days[dayIndex]!;
+      const day = dayIndex;
 
       // Generate the structure for each day
       const dayElement = (
@@ -34,12 +36,12 @@ const TimeTable: React.FC = () => {
             {day}
           </div>
           {/* Container for the content */}
-          <div className="absolute z-10 w-full px-1 py-[0.125rem]">
-            {generateEventsForDay(day)}
-          </div>
+          {/* <div className="absolute z-10 w-full px-1 py-[0.125rem]">
+            <div className="relative">{generateEventsForDay(dayIndex)}</div>
+          </div> */}
           {/* Rows for the day */}
           <div className="absolute z-0 box-border w-full min-w-fit border-x border-b border-gray-400 opacity-30">
-            {generateDayRows()}
+            {generateDayRows(dayIndex)}
           </div>
         </div>
       );
@@ -51,7 +53,7 @@ const TimeTable: React.FC = () => {
   };
 
   // Function to generate the rows for each day
-  const generateDayRows = () => {
+  const generateDayRows = (dayIndex: number) => {
     const dayRowElements = [];
 
     // Loop through each row
@@ -71,33 +73,39 @@ const TimeTable: React.FC = () => {
             className={`col-span-1 box-border h-8 min-w-[6rem] border-t border-gray-500 text-center ${styleClass}`}
           >
             {/* Display events in the cell */}
-            {generateEventsForCell(i, days[j]!)}
+            {generateEventsForCell(i, j)}
           </div>,
         );
       }
 
       // Wrap the columns in a row element
-      dayRowElements.push(<div key={i}>{colElements}</div>);
+      dayRowElements.push(
+        <div key={i} className="grid grid-cols-1 grid-rows-2">
+          {generateEventsForCell(i, dayIndex)}
+          {colElements}
+        </div>,
+      );
     }
 
     return dayRowElements;
   };
 
   // Function to generate events for a specific day
-  const generateEventsForDay = (day: string) => {
-    const dayEvents = events.filter((event) => event.day === day);
-    return dayEvents.map((event, index) => (
-      <TableItem key={index} event={event} />
-    ));
-  };
+  // const generateEventsForDay = (day: number) => {
+  //   const dayEvents = events.filter((event) => getDay(event.startTime) === day);
+  //   return dayEvents.map((event, index) => (
+  //     <TableItem key={index} reservationId={event.id} />
+  //   ));
+  // };
 
   // Function to generate events for a specific cell
-  const generateEventsForCell = (row: number, day: string) => {
+  const generateEventsForCell = (row: number, day: number) => {
     const cellEvents = events.filter(
-      (event) => event.day === day && event.row === row,
+      (event) =>
+        getDay(event.startTime) === day && getReservationHourRow(event) === row,
     );
     return cellEvents.map((event, index) => (
-      <TableItem key={index} event={event} />
+      <TableItem key={index} reservationId={event.id} />
     ));
   };
 
@@ -141,14 +149,35 @@ const TimeTable: React.FC = () => {
     return timeSlots;
   };
 
+  useEffect(() => {
+    addEvent({
+      id: "event-id",
+      machine: {
+        id: "machine-id",
+        name: "machine 1",
+      },
+      team: {
+        id: "team-id",
+        name: "team 1",
+        members: [],
+        projects: [],
+      },
+      startTime: set(parse("2023-12-12", "yyyy-MM-dd", new Date()), {
+        hours: 9,
+        minutes: 0,
+      }),
+      duration: 50,
+    });
+  }, []);
+
   return (
     <div
-      className="m-4 flex w-full  flex-col rounded-lg border border-foreground bg-transparent p-6 outline-none"
+      className="m-auto flex w-full flex-col rounded-lg border border-foreground bg-transparent p-6 outline-none"
       role="tabpanel"
       tabIndex={0}
     >
       <div className="flex h-fit w-full min-w-fit overflow-visible">
-        <div className="sticky left-0 z-10 flex h-full w-16 min-w-[54px] select-none flex-col rounded-t-lg rounded-bl-lg bg-primary p-2 text-primary-foreground">
+        <div className="sticky left-0 z-10 flex h-full min-w-fit max-w-[18rem] select-none flex-col rounded-t-lg rounded-bl-lg bg-primary px-4 py-2 text-primary-foreground">
           {generateTimeSlots()}
         </div>
         <div className="grid h-full w-full min-w-fit grid-cols-10">
