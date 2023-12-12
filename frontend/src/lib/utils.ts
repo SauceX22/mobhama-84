8881558e64bcfc4a2c1edcb72205d0d8a6e162f1
SauceX <mahmoudzak4@gmail.com>
2023-12-12 19:39:01 +0300

    chore: timetable helpers

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,5 +1,7 @@
+import { type Reservation } from "@/server/api"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { getHours } from "date-fns"
 
 
 export function cn(...inputs: ClassValue[]) {
@@ -8,4 +10,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`
+}
+
+function getReservationHourRowWithStartTime(reservation: Reservation, tableStartTime: number) {
+  return getHours(reservation.startTime) - tableStartTime;
+}
+
+export function getReservationHourRow(reservation: Reservation) {
+  return getReservationHourRowWithStartTime(reservation, 7)
 }
