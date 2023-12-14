1564b69d00c21fa6f5c5edd2f558ec4d1c841ca0
SauceX <mahmoudzak4@gmail.com>
2023-12-14 03:23:12 +0300

    fix: login by name

--- frontend/src/lib/session.ts
+++ frontend/src/lib/session.ts
@@ -1,6 +1,6 @@
-import { getServerSession } from "next-auth/next"
+import { getServerSession } from "next-auth/next";
 
-import { authOptions } from "@/lib/auth"
+import { authOptions } from "@/lib/auth";
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
@@ -10,13 +10,7 @@ import { authOptions } from "@/lib/auth"
 export const getServerAuthSession = () => getServerSession(authOptions);
 
 export async function getCurrentUser() {
-    const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-    return session?.user ?? {
-        id: "13651651ads6f51as65df65asd6f4a",
-        name: "some name",
-        email: "email@emailexample.com",
-        image: "https://avatars.githubusercontent.com/u/13651651?v=4",
-    }
+  return session?.user;
 }
-
